3755e29101ef16b43cb4f3b5faee4c5ee28c6aeb
Danny van Os <dannyvanos93@gmail.com>
2020-11-08 16:39:42 +0100

    Added genre home
    Also removed the section for head-nav to include it at all times

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,30 +1,26 @@
 @extends('layouts.app')
 
-@section('head-nav')
-	@include('layouts.partials.head-nav')
-@endsection
-
 @section('main')
-@auth
-    Welcome {{ auth()->user()->name }}    
-@endauth
+    @auth
+        Welcome {{ auth()->user()->name }}    
+    @endauth
 
-<ul>
-    <li>Show latest added songs</li>
-    <li>Show latest added albums</li>
-    <li>Show latest added artist</li>
-    <li>Show list of genres</li>
-</ul>
+    <ul>
+        <li>Show latest added songs</li>
+        <li>Show latest added albums</li>
+        <li>Show latest added artist</li>
+        <li>Show list of genres</li>
+    </ul>
 
-<div class="flex flex-wrap">
-    <div class="w-5 h-6 bg-red-100"></div>
-    <div class="w-5 h-6 bg-red-200"></div>
-    <div class="w-5 h-6 bg-red-300"></div>
-    <div class="w-5 h-6 bg-red-400"></div>
-    <div class="w-5 h-6 bg-red-500"></div>
-    <div class="w-5 h-6 bg-red-600"></div>
-    <div class="w-5 h-6 bg-red-700"></div>
-    <div class="w-5 h-6 bg-red-800"></div>
-    <div class="w-5 h-6 bg-red-900"></div>
-</div>
+    <div class="flex flex-wrap">
+        <div class="w-5 h-6 bg-red-100"></div>
+        <div class="w-5 h-6 bg-red-200"></div>
+        <div class="w-5 h-6 bg-red-300"></div>
+        <div class="w-5 h-6 bg-red-400"></div>
+        <div class="w-5 h-6 bg-red-500"></div>
+        <div class="w-5 h-6 bg-red-600"></div>
+        <div class="w-5 h-6 bg-red-700"></div>
+        <div class="w-5 h-6 bg-red-800"></div>
+        <div class="w-5 h-6 bg-red-900"></div>
+    </div>
 @endsection
