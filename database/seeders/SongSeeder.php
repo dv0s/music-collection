1145600521bce2f0c3ac0e3d3f791ac8a045e27d
Danny van Os <dannyvanos93@gmail.com>
2020-11-14 14:45:57 +0100

    Added song seeder
    Created the seeder for faster populating of the database with new songs.

---
+++ database/seeders/SongSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Song;
+use Illuminate\Database\Seeder;
+
+class SongSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Song::factory()
+                ->times(500)
+                ->create();
+    }
+}
