53b0de688b95fe3155836830f5ae05b583d15a0d
Danny van Os <dannyvanos93@gmail.com>
2020-11-19 00:36:06 +0100

    Created notifications

---
+++ resources/views/notifications/warning.blade.php
@@ -0,0 +1,4 @@
+<div class="flex flex-warp w-full bg-yellow-200 text-yellow-600 mb-6 px-3 py-4 border border-yellow-300">
+    <svg class="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+    <span>{{ $message ?? "" }}</span>
+</div>
